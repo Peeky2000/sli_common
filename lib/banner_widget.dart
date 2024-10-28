16267ee3dc4fca78c43d2228dff884a12fc9dd30
Long Nguyễn <long.nh16hn@gmail.com>
2024-10-28 17:15:26 +0700

    fix has size banner

--- lib/banner_widget.dart
+++ lib/banner_widget.dart
@@ -20,7 +20,7 @@ class BannerWidget extends StatefulWidget {
   final Function(int)? onTap;
   final Widget? errorWidget;
   final BoxFit? fit;
-  final bool? dotUnder;
+  final bool dotUnder;
 
   const BannerWidget({
     Key? key,
@@ -87,77 +87,79 @@ class _BannerWidgetState extends State<BannerWidget> {
       height: widget.height ?? 110.0.h,
       child: Column(
         children: [
-          Stack(
-            alignment: Alignment.bottomCenter,
-            children: [
-              InkWell(
-                onTap: widget.onTap == null
-                    ? null
-                    : () {
-                        if (widget.onTap != null) {
-                          widget.onTap!(currentIndex);
-                        }
-                      },
-                child: PageView.builder(
-                  controller: pageController,
-                  physics: widget.physics,
-                  onPageChanged: (index) {
-                    if (widget.onChange != null) {
-                      widget.onChange!(index);
-                    }
-                    setState(() {
-                      currentIndex = index;
-                    });
-                  },
-                  scrollDirection: Axis.horizontal,
-                  itemCount: widget.urls.length,
-                  itemBuilder: (context, index) => ClipRRect(
-                    borderRadius: BorderRadius.circular(widget.radius ?? 8.0.r),
-                    child: CachedNetworkImage(
-                      imageUrl: widget.urls[index],
-                      placeholder: (context, url) => const ImageLoading(),
-                      fit: widget.fit ?? BoxFit.cover,
-                      errorWidget: (context, url, error) =>
-                          widget.errorWidget ?? const SizedBox(),
+          Expanded(
+            child: Stack(
+              alignment: Alignment.bottomCenter,
+              children: [
+                InkWell(
+                  onTap: widget.onTap == null
+                      ? null
+                      : () {
+                          if (widget.onTap != null) {
+                            widget.onTap!(currentIndex);
+                          }
+                        },
+                  child: PageView.builder(
+                    controller: pageController,
+                    physics: widget.physics,
+                    onPageChanged: (index) {
+                      if (widget.onChange != null) {
+                        widget.onChange!(index);
+                      }
+                      setState(() {
+                        currentIndex = index;
+                      });
+                    },
+                    scrollDirection: Axis.horizontal,
+                    itemCount: widget.urls.length,
+                    itemBuilder: (context, index) => ClipRRect(
+                      borderRadius: BorderRadius.circular(widget.radius ?? 8.0.r),
+                      child: CachedNetworkImage(
+                        imageUrl: widget.urls[index],
+                        placeholder: (context, url) => const ImageLoading(),
+                        fit: widget.fit ?? BoxFit.cover,
+                        errorWidget: (context, url, error) =>
+                            widget.errorWidget ?? const SizedBox(),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              if (widget.urls.length > 1)
-                widget.dotUnder == true
-                    ? SizedBox.shrink()
-                    : Padding(
-                        padding: EdgeInsets.all(8.0.w),
-                        child: SingleChildScrollView(
-                          scrollDirection: Axis.horizontal,
-                          child: DotsIndicator(
-                              dotsCount: widget.urls.length,
-                              position: currentIndex.toDouble(),
-                              onTap: (index) => pageController?.animateToPage(
-                                  index.toInt(),
-                                  duration: const Duration(milliseconds: 400),
-                                  curve: Curves.easeInOut),
-                              decorator: DotsDecorator(
-                                animationDuration:
-                                    const Duration(milliseconds: 400),
-                                color: widget.unActiveColor ??
-                                    Colors.white.withOpacity(0.5),
-                                activeColor: widget.activeColor ??
-                                    const Color(0xFFF35A49),
-                                size: Size.square(9.0.w),
-                                activeSize: Size(18.0.w, 9.0.h),
-                                activeShape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(5.0.r),
-                                ),
-                              )),
-                        ),
-                      )
-            ],
+                if (widget.urls.length > 1)
+                  widget.dotUnder == true
+                      ? SizedBox.shrink()
+                      : Padding(
+                          padding: EdgeInsets.all(8.0.w),
+                          child: SingleChildScrollView(
+                            scrollDirection: Axis.horizontal,
+                            child: DotsIndicator(
+                                dotsCount: widget.urls.length,
+                                position: currentIndex.toDouble(),
+                                onTap: (index) => pageController?.animateToPage(
+                                    index.toInt(),
+                                    duration: const Duration(milliseconds: 400),
+                                    curve: Curves.easeInOut),
+                                decorator: DotsDecorator(
+                                  animationDuration:
+                                      const Duration(milliseconds: 400),
+                                  color: widget.unActiveColor ??
+                                      Colors.white.withOpacity(0.5),
+                                  activeColor: widget.activeColor ??
+                                      const Color(0xFFF35A49),
+                                  size: Size.square(9.0.w),
+                                  activeSize: Size(18.0.w, 9.0.h),
+                                  activeShape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(5.0.r),
+                                  ),
+                                )),
+                          ),
+                        )
+              ],
+            ),
           ),
-          SizedBox(height: 3.0.h),
+          //SizedBox(height: 3.0.h),
           widget.urls.length > 1 && widget.dotUnder == true
               ? Padding(
-                  padding: EdgeInsets.all(8.0.w),
+                  padding: EdgeInsets.all(4.0.w),
                   child: SingleChildScrollView(
                     scrollDirection: Axis.horizontal,
                     child: DotsIndicator(
